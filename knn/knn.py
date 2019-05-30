587a0fe304ecbd853959cd9377567b5097371e95
Peter <peter@Mings-MacBook-Air.local>
2019-05-30 09:54:05 +0800

    knn update

--- knn/knn.py
+++ knn/knn.py
@@ -1,5 +1,7 @@
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -21,7 +23,6 @@ def classify0(inX, dataSet, labels, k):
 
     return sortedClassCount[0][0]
 
-
 def file2matrix (filename):
     fr = open (filename)
     arrayOLines = fr.readlines()
@@ -37,6 +38,39 @@ def file2matrix (filename):
         index += 1
     return returnMat, classLabelVector
 
+#Normalize the data set
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet, ranges, minVals
 
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('dating.txt')
+    normMat, ranges, minVals = autoNorm (datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range (numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m,:],  
+            datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is %d" \
+            % (classifierResult, datingLabels[i]) 
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print "the total error rate is %f" % (errorCount/float( numTestVecs))
 
+def showImage():
+    datingDataMat, datingLabels = file2matrix('dating.txt')
+    normMat, ranges, minVals = autoNorm (datingDataMat)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(normMat[:,2], normMat[:,0], 
+        15.0*array(datingLabels), 15.0*array(datingLabels))
+    plt.show()
 
